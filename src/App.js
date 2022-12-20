cb620c658d0f381b254181aa569b052d6f174fc9
Yuri Padilla <yuripadilla017@gmail.com>
2022-12-20 14:03:36 +0100

    create form

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from "react";
+
+function Form({onAddActivity}) {
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    const formData = new FormData(event.target);
+    const data = Object.fromEntries(formData);
+
+    console.log(event.target.elements.isForGoodWeather.checked);
+
+    const isForGoodWeather = event.target.elements.isForGoodWeather.checked;
+    onAddActivity({name: data.name, isForGoodWeather });
+
+    event.target.reset();
+    event.target.elements.name.focus();
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h1>Add new Activity</h1>
+      <label htmlFor="input-name">Name</label>
+      <input id="input-name" type="text" name="name"/>
+
+      <label htmlFor="input-checkBox">Good-weather activity</label>
+      <input id="input-checkBox" type="checkbox" name="isForGoodWeather"/>
+
+      <button type="submit">Submit</button>
+    </form>
+  );
+}
 
 function App() {
+  const [activities, setActivities] = useState([]);
+
+  function handleAddActivity(newActivity) {
+    setActivities([newActivity, ...activities]);
+
+    console.log(newActivity);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Form onAddActivity={handleAddActivity}/>
+    </>
   );
 }
 
